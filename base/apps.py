69b98a72f54965a31186344b379eb08a69ed745a
Almas <v.azaurbekov@gmail.com>
2023-05-24 15:21:13 +0600

    base app was added

--- base/apps.py
+++ base/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SkylearnwebsiteConfig(AppConfig):
+class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'skylearnwebsite'
+    name = 'base'
---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Room(models.Model):
+    # host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self) -> str:
+        return self.name
